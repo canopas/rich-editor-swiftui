ca4f8d9b893a429662947f22280725bb57ae710e
Divyesh Canopas <83937721+cp-divyesh-v@users.noreply.github.com>
2023-10-11 14:52:30 +0530

    Add demo app (#1)
    * added demo app
    
    * Updated name of package

--- Tests/RichEditorSwiftuiTests/RichEditorSwiftUITests.swift
+++ Tests/RichEditorSwiftuiTests/RichEditorSwiftUITests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import RichEditorSwiftui
+@testable import RichEditorSwiftUI
 
-final class RichEditorSwiftuiTests: XCTestCase {
+final class RichEditorSwiftUITests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
